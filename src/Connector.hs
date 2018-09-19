da445328acad9289a7ac85a75fb25d6707c9c889
niamtokik <mk@steepath.eu>
2018-09-19 11:32:46 +0200

    add multithreading layer

--- src/Connector.hs
+++ src/Connector.hs
@@ -13,39 +13,11 @@ import Data.ByteString.Lazy.Char8
 
 import Control.Concurrent
 import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
 import Control.Concurrent.STM.TChan
 
-
-data Process = Process { sock :: IO Network.Socket
-                       , input :: STM (TChan String)
-                       , output :: STM (TChan String)
-                       }
-
-createState = 
-  Process { sock = initConnection "localhost" "7779"
-          , input = newTChan
-          , output = newTChan
-          }
-
-createProcess = forkIO $ process $ createState
-
-process state = do
-  let Process { sock = s, input = i, output = o } = state
-  ss <- s
-  _ <- sendData ss "test"
-  Prelude.putStrLn $ show ss
-  process state
-
-processRead input = do
-  i <- input
-  -- _ <- readTChan i
-  return $ Prelude.putStr "read"
-
-processWrite output = do
-  o <- output
-  -- _ <- writeTChan o
-  return $ Prelude.putStr "write"
-
+import GHC.IO.Handle
+import GHC.IO.IOMode
 
 init :: IO Network.Socket
 init = initConnection "localhost" "7777"
@@ -58,6 +30,65 @@ initConnection host port = do
   -- Network.Socket.setSocketOption s KeepAlive 1
   connect s $ Network.Socket.addrAddress addr
   return s
+
+-- This function return an handler instead of a socket
+testConn host port = do
+  let hints = defaultHints { Network.Socket.addrSocketType = Stream }
+  addr:_ <- Network.Socket.getAddrInfo (Just hints) (Just host) (Just port)
+  s <- Network.Socket.socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  connect s $ Network.Socket.addrAddress addr
+  h <- Network.Socket.socketToHandle s ReadWriteMode
+  hSetBuffering h NoBuffering
+  GHC.IO.Handle.hPutStr h "test"
+  return h 
+
+-- main data structure used: one thread to read and another
+-- one to write data. We also share STM queue
+data Container = Container { readThread :: ThreadId
+                           , writeThread :: ThreadId
+                           , readQueue :: STM( TQueue String)
+                           , writeQueue :: STM (TQueue String)
+                           }
+
+-- create a connection and return container
+-- data structure
+createClientConnection host port = do
+  createContainer $ testConn host port
+
+-- create a container with thread initialization
+createContainer :: IO Handle -> IO Container
+createContainer handler = do
+  let rq = newTQueue
+  let wq = newTQueue
+  h <- handler
+  rt <- forkIO $ testReceive h rq
+  wt <- forkIO $ testSend h wq
+  return $ Container { readThread = rt
+                     , writeThread = wt
+                     , readQueue = rq
+                     , writeQueue = wq
+                     }
+    
+-- read message from queue and send it to handler
+testSend handler queue = do
+  q <- atomically $ queue
+  message <- atomically $ readTQueue q
+  GHC.IO.Handle.hPutStr handler message
+  testSend handler queue
+
+-- read message from handler and write it to the queue
+testReceive handler queue = do
+  q <- atomically $ queue
+  message <- GHC.IO.Handle.hGetLine handler
+  Prelude.putStrLn message
+  atomically $ writeTQueue q message
+  testReceive handler queue
+
+-- send test function
+s Container { writeQueue = wq } str = do
+  q <- atomically wq
+  atomically $ writeTQueue q str
+  
 
 toBert d = Data.ByteString.Lazy.toStrict $ Data.Binary.encode . Data.BERT.showBERT $ d
 
